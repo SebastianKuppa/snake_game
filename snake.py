d329c9d91252915775cb26f670d5c04aeef823ac
test <sebastian.kuppa@web.de>
2023-01-21 18:08:20 +0800

    added respawn function

--- snake.py
+++ snake.py
@@ -18,3 +18,9 @@ class Snake:
         self.block_size = block_size
         self.bounds = bounds
         self.respawn()
+
+    def respawn(self):
+        self.length = 3
+        self.body = [(20, 20), (20, 40), (20, 60)]
+        self.direction = Direction.DOWN
+
