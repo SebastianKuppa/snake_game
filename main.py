a4c232ca04884f322cf5fe97e92bbb380418c667
test <sebastian.kuppa@web.de>
2023-01-22 15:19:16 +0800

    added game loop

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from snake import Snake
 import pygame
 
+# game params
+bounds = (300, 300)
+block_size = 20
+run = True
+game_speed = 100  # in ms
 
 if __name__ == '__main__':
-    bounds = (300, 300)
     pygame.init()
     window = pygame.display.set_mode(bounds)
     pygame.display.set_caption('SNAKE')
-    player = Snake(block_size=20, bounds=bounds)
+    snake = Snake(block_size=block_size, bounds=bounds)
+    # gaming loop
+    while run:
+        pygame.time.delay(game_speed)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            window.fill(color=(0, 0, 0))
+            snake.draw(pygame, window)
+            pygame.display.flip()
 
