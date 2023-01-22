e7b959b2b7316de019f7e2e7806801cf220c1323
test <sebastian.kuppa@web.de>
2023-01-22 15:01:40 +0800

    init player in window

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import snake
+from snake import Snake
 import pygame
 
 
-pygame.init()
-window = pygame.display.set_mode((300, 300))
-pygame.display.set_caption('SNAKE')
+if __name__ == '__main__':
+    bounds = (300, 300)
+    pygame.init()
+    window = pygame.display.set_mode(bounds)
+    pygame.display.set_caption('SNAKE')
+    player = Snake(block_size=20, bounds=bounds)
 
