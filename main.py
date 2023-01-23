ba436129c707c23143f163dd10959d4cc14e59f2
test <sebastian.kuppa@web.de>
2023-01-23 17:43:55 +0800

    added comments

--- main.py
+++ main.py
@@ -9,21 +9,31 @@ run = True
 game_speed = 100  # in ms
 
 if __name__ == '__main__':
+    # start mygame instance
     pygame.init()
+    # init game window
     window = pygame.display.set_mode(bounds)
+    # set window name
     pygame.display.set_caption('SNAKE')
+    # init Snake object
     snake = Snake(block_size=block_size, bounds=bounds)
+    # add clock object for window update
     clock = pygame.time.Clock()
-    # gaming loop
+    # start gaming loop
     while run:
+        # how fast to update the game screen
         pygame.time.delay(game_speed)
-        # time.sleep(3)
+        # closing the window in the top right corner will stop the loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+        # new snake position/length is calculated
         snake.move()
+        # clear game window to black
         window.fill(color=(0, 0, 0))
+        # draw the new snake, after move() method was called
         snake.draw(pygame, window)
+        # update window
+        # update window
         pygame.display.update()
         clock.tick(40)
