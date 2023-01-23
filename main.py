2739c251f165e1bae1b54a2ec035e3f35fb94a6f
test <sebastian.kuppa@web.de>
2023-01-23 12:20:34 +0800

    WIP window updating bug

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import pygame
 bounds = (300, 300)
 block_size = 20
 run = True
-game_speed = 3000  # in ms
+game_speed = 10  # in ms
 
 if __name__ == '__main__':
     pygame.init()
     window = pygame.display.set_mode(bounds)
     pygame.display.set_caption('SNAKE')
     snake = Snake(block_size=block_size, bounds=bounds)
+    clock = pygame.time.Clock()
     # gaming loop
     while run:
         pygame.time.delay(game_speed)
@@ -20,7 +21,7 @@ if __name__ == '__main__':
                 run = False
 
             snake.move()
-            # window.fill(color=(0, 0, 0))
+            window.fill(color=(0, 0, 0))
             snake.draw(pygame, window)
-            pygame.display.flip()
-
+            pygame.display.update()
+            clock.tick(1)
